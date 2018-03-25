58332c17a3a084db5f9887787360850d0ad37206
Azure Zanculmarktum <zanculmarktum@gmail.com>
2018-03-25 22:08:31 +0700

    Set new page and start page to about:blank

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -11,3 +11,5 @@ c.fonts.web.family.standard = None
 c.fonts.web.size.default = 12
 c.fonts.web.size.default_fixed = 12
 c.fonts.web.size.minimum = 12
+c.url.default_page = 'about:blank'
+c.url.start_pages = c.url.default_page
