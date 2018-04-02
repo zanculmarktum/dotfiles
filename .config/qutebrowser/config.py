9701ccc0a7781e5321bcc7b74beef60154d8e473
Azure Zanculmarktum <zanculmarktum@gmail.com>
2018-04-03 06:07:56 +0700

    Added searx

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,9 +1,9 @@
 # Uncomment this to still load settings configured via autoconfig.yml
 # config.load_autoconfig()
 
-c.backend = 'webkit'
+#c.backend = 'webkit'
 c.content.developer_extras = True
-c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'g': 'https://www.google.com/search?q={}'}
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'g': 'https://www.google.com/search?q={}', 'sx': 'https://www.searx.me/search?q={}'}
 c.fonts.web.family.fixed = '"DejaVu Sans Mono"'
 c.fonts.web.family.sans_serif = 'Cantarell'
 c.fonts.web.family.serif = 'Cantarell'
