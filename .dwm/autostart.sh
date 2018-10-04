7ca7a0022461c30eac2ad1d8c3375e6a4c1f19c1
Azure Zanculmarktum <zanculmarktum@gmail.com>
2018-10-04 21:59:59 +0700

    add -P option

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 hsetroot -center $(cd "${0%/*}"; printf "$(pwd)"'/%s\n' wall.* | head -1) &
-redshift-gtk &
+redshift-gtk -P &
 parcellite &
 conky | while read line; do xsetroot -name "$line"; done &
