c417ebe1dfcb7a81ba47fbc147c351b73184eba6
Azure Zanculmarktum <zanculmarktum@gmail.com>
2019-04-29 19:19:33 +0700

    hibernate on 10 minutes idle

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 hsetroot -center $(cd "${0%/*}"; printf "$(pwd)"'/%s\n' wall.* | head -1) &
-redshift-gtk -P &
+redshift-gtk &
 parcellite &
 conky | while read line; do xsetroot -name "$line"; done &
+xautolock -time 10 -locker 'systemctl hibernate' &
