d824d4ba819aaf635c9e2a5ee6f69777c88a4689
Azure Zanculmarktum <zanculmarktum@gmail.com>
2019-04-30 19:52:07 +0700

    don't start locker if mouse is on bottom left

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,4 +4,4 @@ hsetroot -center $(cd "${0%/*}"; printf "$(pwd)"'/%s\n' wall.* | head -1) &
 redshift-gtk &
 parcellite &
 conky | while read line; do xsetroot -name "$line"; done &
-xautolock -time 10 -locker 'systemctl hibernate' &
+xautolock -time 10 -locker 'systemctl hibernate' -corners 000- -cornersize 30 &
