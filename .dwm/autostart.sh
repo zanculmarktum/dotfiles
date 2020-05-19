ab56cbcd631c02d9077b08c70b2b3bfdce7905c4
Azure Zanculmarktum <zanculmarktum@gmail.com>
2020-05-19 21:34:48 +0700

    add i3lock-fancy to xautolock

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,4 +4,4 @@ hsetroot -center $(cd "${0%/*}"; printf "$(pwd)"'/%s\n' wall.* | head -1) &
 redshift-gtk &
 parcellite &
 conky | while read line; do xsetroot -name "$line"; done &
-xautolock -time 10 -locker 'systemctl hibernate' -corners 000- -cornersize 30 &
+xautolock -time 5 -locker 'i3lock-fancy & { sleep 300; systemctl hibernate; }' -corners 000- -cornersize 30 &
