926b57edda4f6d0edbef6d03973376d7a4579326
Azure Zanculmarktum <zanculmarktum@gmail.com>
2020-05-24 21:27:58 +0700

    fix xautolock -locker

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,4 +4,16 @@ hsetroot -center $(cd "${0%/*}"; printf "$(pwd)"'/%s\n' wall.* | head -1) &
 redshift-gtk &
 parcellite &
 conky | while read line; do xsetroot -name "$line"; done &
-xautolock -time 5 -locker 'i3lock-fancy & { sleep 300; systemctl hibernate; }' -corners 000- -cornersize 30 &
+xautolock -time 5 -locker 'i3lock-fancy &
+i=1
+while :; do
+  sleep 10
+  if ! pidof i3lock >/dev/null 2>&1; then
+    break
+  fi
+  if [ $i -eq 30 ]; then
+    systemctl hibernate
+    break
+  fi
+  i=$(($i+1))
+done' -corners 000- -cornersize 30 &
