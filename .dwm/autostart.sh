602ce569e48f5a9d06a40f29920c3077a1d60f05
Azure Zanculmarktum <zanculmarktum@gmail.com>
2022-11-30 02:21:21 +0700

    disable i3lock-fancy

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-hsetroot -center $(cd "${0%/*}"; printf "$(pwd)"'/%s\n' wall.* | head -1) &
+hsetroot -fill $(cd "${0%/*}"; printf "$(pwd)"'/%s\n' wall.* | head -1) &
 redshift-gtk &
 parcellite &
+compton &
 conky | while read line; do xsetroot -name "$line"; done &
-xautolock -time 5 -locker 'i3lock-fancy &
-i=1
-while :; do
-  sleep 10
-  if ! pidof i3lock >/dev/null 2>&1; then
-    break
-  fi
-  if [ $i -eq 30 ]; then
-    systemctl hibernate
-    break
-  fi
-  i=$(($i+1))
-done' -corners 000- -cornersize 30 &
+#xautolock -time 5 -locker 'i3lock-fancy &
+#i=1
+#while :; do
+#  sleep 10
+#  if ! pidof i3lock >/dev/null 2>&1; then
+#    break
+#  fi
+#  if [ $i -eq 30 ]; then
+#    systemctl hibernate
+#    break
+#  fi
+#  i=$(($i+1))
+#done' -corners 000- -cornersize 30 &
