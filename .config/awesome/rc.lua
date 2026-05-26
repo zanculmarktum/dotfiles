428013eac7bc2a9ac96960fdc75ebc52a3b34aee
Azure Zanculmarktum <zanculmarktum@gmail.com>
2026-05-26 19:37:43 +0700

    disable mouse scrolling

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -179,8 +179,8 @@ screen.connect_signal("request::desktop_decoration", function(s)
                                                 client.focus:toggle_tag(t)
                                             end
                                         end),
-            awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
-            awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end),
+            -- awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
+            -- awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end),
         }
     }
 
